0ab5f86138b1fee9934115a50ec7e31b9f879e38
marvin <marnyanjui@gmail.com>
2022-04-24 09:19:37 +0300

    Add Min_max prototype

--- Max-Min/Max_Min.cpp
+++ Max-Min/Max_Min.cpp
@@ -1,12 +1,16 @@
-//
 // Created by nyanjuimarvin on 24/04/22.
-//
+
+/* Find the maximum and minimum values in a given array of Integers */
 
 #include <iostream>
 #include <array>
 #include <cstddef>
 
+void find_Max_Min(int array1[],size_t size, int *min, int* max);
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+

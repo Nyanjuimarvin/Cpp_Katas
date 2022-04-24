3500d26a94b772d92c8ee5d526dd3371ff053b23
marvin <marnyanjui@gmail.com>
2022-04-24 09:26:40 +0300

    Add find_Max_Min definition

--- Max-Min/Max_Min.cpp
+++ Max-Min/Max_Min.cpp
@@ -6,11 +6,25 @@
 #include <array>
 #include <cstddef>
 
-void find_Max_Min(int array1[],size_t size, int *min, int* max);
+void find_Max_Min(const int givenArray[],size_t size, int *min, int* max);
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
 
+void find_Max_Min(const int givenArray[], size_t size, int *minimum, int *maximum){
 
+    for(size_t i = 1; i < size; i++){
+        if(givenArray[i] > *maximum){
+            *maximum = givenArray[i];
+        }
+
+        if (givenArray[i] < *minimum){
+            *minimum = givenArray[i];
+        }
+    }
+
+    std::cout << "Minimum is: %d" << minimum << std::endl;
+    std::cout << "Maximum is %d" << maximum << std::endl;
+}

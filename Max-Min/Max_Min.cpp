2d665e61ff6767146e0cc612a03963f7f1880a14
marvin <marnyanjui@gmail.com>
2022-04-24 09:11:21 +0300

    Add max_min

---
+++ Max-Min/Max_Min.cpp
@@ -0,0 +1,12 @@
+//
+// Created by nyanjuimarvin on 24/04/22.
+//
+
+#include <iostream>
+#include <array>
+#include <cstddef>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
